606265e7a0cdce38275e9e9fdd37e83448777d6c
Edwin Ginet <edwinginet@gmail.com>
2022-07-21 19:58:45 +0200

    Third commit, Modified 'FilmLength' -> 'Filmgenre', 'Schedule' -> 'Session_Schedule', 'Seats' -> 'Remaining_Seats'

--- Evaluation-BDD.sql
+++ Evaluation-BDD.sql
@@ -5,16 +5,18 @@ CREATE TABLE theaters (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     Name VARCHAR(30) NOT NULL,
     Address VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE theater_sessions (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     FilmTitle VARCHAR(30) NOT NULL,
-    Schedule DATE NOT NULL,
-    FilmLength TIME NOT NULL,
-    Seats INTEGER NOT NULL,
+    Session_Schedule DATETIME NOT NULL,
+    FilmGenre VARCHAR(30) NOT NULL,
+    Remaining_Seats INTEGER NOT NULL,
+    FOREIGN KEY (id) REFERENCES theaters(id),
+    FOREIGN KEY (id) REFERENCES users(id)
 
-)
+);
 
 CREATE TABLE tickets (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -22,18 +24,18 @@ CREATE TABLE tickets (
     Price FLOAT NOT NULL,
     Booking DATETIME NOT NULL,
 
-)
+);
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     Username VARCHAR(30) NOT NULL,
     Password VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE added_sessions (
     Date DATETIME,
 
-)
+);
 
 CREATE TABLE customers (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -42,4 +44,4 @@ CREATE TABLE customers (
     Email VARCHAR(30) NOT NULL,
     LastName VARCHAR(30) NOT NULL,
     FirstName VARCHAR(30) NOT NULL
-)
+);
